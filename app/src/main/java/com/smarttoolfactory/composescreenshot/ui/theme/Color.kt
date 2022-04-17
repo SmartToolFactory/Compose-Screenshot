31a5ace19bfb937e6932fed3225ddd0c18c9d7bc
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-04-17 15:59:51 +0300

    add colors and sample jpeg

--- app/src/main/java/com/smarttoolfactory/composescreenshot/ui/theme/Color.kt
+++ app/src/main/java/com/smarttoolfactory/composescreenshot/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val BlueSmart = Color(0xff29b6f6)
+val BlueSmartDark = Color(0xff03a9f4)
